24679365cc035e19aba0008b0fdd5495b12d0419
Krista Ghazaleh <kristaelizabethjen@gmail.com>
2020-01-15 12:43:34 -0800

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,10 @@
+#author: Krista Elizabeth Ghazaleh
+#date: 1/14/2020
+#SID: 0539027
+#assignment: 1
+#description: Console Output
+print('X    X    OOO')
+print(' X  X    O   O')
+print('   X    O     O')
+print(' X   X   O   O')
+print('X     X   OOO')
