bd85db9378b082dadd29cd5252fb8f1846fe07ee
Krista Ghazaleh <kristaelizabethjen@gmail.com>
2020-01-15 12:44:06 -0800

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,8 +1,3 @@
-#author: Krista Elizabeth Ghazaleh
-#date: 1/14/2020
-#SID: 0539027
-#assignment: 1
-#description: Console Output
 print('X    X    OOO')
 print(' X  X    O   O')
 print('   X    O     O')
