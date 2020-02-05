f959919e248bb0ce1a7235a51cfba967b02eb37a
Elle Grace Jen <kristaelizabethjen@gmail.com>
2020-02-04 20:30:35 -0800

    Update random.py

--- random.py
+++ random.py
@@ -7,3 +7,10 @@ int sumOdd = 0
 
 for i in range(0, 20) :
   x = random.randint(1, 100)
+  if x % 2 == 0:
+    sumEven += x
+  else:
+    sumOdd += x
+    
+print('even: ', sumEven)
+print('odd: ', sumOdd)
